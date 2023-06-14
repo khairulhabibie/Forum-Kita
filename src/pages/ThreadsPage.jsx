d5d2714def4f78a09647609db6b906bf9a2d7c87
khairul habibie <96696443+khairulhabibie@users.noreply.github.com>
2023-06-14 01:59:54 +0000

    berhasil mengambil data dengan redux

--- src/pages/ThreadsPage.jsx
+++ src/pages/ThreadsPage.jsx
@@ -3,16 +3,20 @@ import React, { useEffect } from 'react';
 // import { useDispatch, useSelector } from 'react-redux';
 // import ThreadList from '../component/ThreadList';
 // import Chategory from '../component/Chategory';
-// import asyncPopulateUserThreadAndLeaderboards from '../state/shared/action';
+import { useDispatch, useSelector } from 'react-redux';
+import asyncPopulateUserThreadAndLeaderboards from '../state/shared/action';
 
 function ThreadsPage() {
+  const dispath = useDispatch();
+  const { users } = useSelector((states) => states);
   useEffect(() => {
-
-  }, []);
+    dispath(asyncPopulateUserThreadAndLeaderboards());
+  }, [dispath]);
 
   return (
     <div className="min-h-screen">
       {/* <Chategory /> */}
+      {console.log(users)}
       <div className="my-5">
         <h3 className="text-2xl font-bold">Disikusi Tersedia</h3>
         {/* <ThreadList thread={threadList} /> */}
